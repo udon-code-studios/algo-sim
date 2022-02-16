905f7fe586c8cc8ebf7842063bc643c2a8f89097
Leo B <leo.battalora@gmail.com>
2022-02-16 11:17:56 -0500

    editor and capture text

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,23 +3,40 @@ import Editor from "@monaco-editor/react";
 
 export default function Index() {
   const [editor, setEditor] = useState(null);
+  const [code, setCode] = useState('');
 
   return (
-    <div>
-      <h1 className='font-mono'>Welcome to Remix</h1>
+    <div className='w-screen flex flex-col items-center space-y-4'>
+      <h1 className='text-4xl font-bold'>Welcome to AlgoSim</h1>
 
-      <button onClick={() => { if (editor) console.log(editor.getValue()) }}>print value</button>
+      <button
+        onClick={() => { if (editor) setCode(editor.getValue()) }}
+        className='w-72 bg-emerald-500 shadow rounded font-mono hover:shadow-lg'
+      >
+        capture editor contents
+      </button>
 
-      <div style={{ width: '600px', height: '500px', }}>
-        <Editor
-          theme='vs-dark'
-          defaultLanguage='javascript'
-          defaultValue='// some comment'
-          onMount={(editor, _) => {
-            setEditor(editor)
-          }}
-        />
+      <div className='flex space-x-4'>
+        {/* code editor */}
+        <div className='w-96 h-96'>
+          <h1 className='pb-2 text-xl'>Code Editor:</h1>
+          <Editor
+            theme='vs-dark'
+            defaultLanguage='javascript'
+            defaultValue='// some comment'
+            onMount={(editor, _) => {
+              setEditor(editor)
+            }}
+          />
+        </div>
+
+        {/* captured text */}
+        <div className='w-96 h-96'>
+          <h1 className='pb-2 text-xl'>Captured Text:</h1>
+          <pre>{code}</pre>
+        </div>
       </div>
+
     </div>
   );
 }
