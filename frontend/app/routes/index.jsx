0d3c61aa957cd0cba48f068b8f05d8671667bfdb
leo6liu <leo.battalora@gmail.com>
2022-02-25 18:03:18 -0500

    home page with link to add page

--- frontend/app/routes/index.jsx
+++ frontend/app/routes/index.jsx
@@ -1,43 +1,14 @@
-import { useRef, useState } from 'react';
-import Editor from "@monaco-editor/react";
+import { Link } from "remix";
 
 export default function Index() {
-  const [editor, setEditor] = useState(null);
-  const [code, setCode] = useState('');
-
   return (
-    <div className='w-screen flex flex-col items-center space-y-4'>
-      
-      <h1 className='text-4xl font-bold'>Welcome to AlgoSim</h1>
-
-      <button
-        onClick={() => { if (editor) setCode(editor.getValue()) }}
-        className='w-72 bg-emerald-500 shadow rounded font-mono hover:shadow-lg'
-      >
-        capture editor contents
-      </button>
-
-      <div className='flex space-x-4'>
-        {/* code editor */}
-        <div className='w-96 h-96'>
-          <h1 className='pb-2 text-xl'>Code Editor:</h1>
-          <Editor
-            theme='vs-dark'
-            defaultLanguage='javascript'
-            defaultValue='// some comment'
-            onMount={(editor, _) => {
-              setEditor(editor)
-            }}
-          />
+    <div className="flex min-h-screen w-full flex-col items-center space-y-4 bg-gray-500">
+      <h1 className="mt-4 text-4xl font-bold">Welcome to AlgoSim!</h1>
+      <Link to="/add">
+        <div className="rounded-xl bg-teal-400 p-4 text-2xl font-bold shadow-2xl duration-200 hover:bg-teal-900 hover:text-teal-200">
+          Click me to add numbers.
         </div>
-
-        {/* captured text */}
-        <div className='w-96 h-96'>
-          <h1 className='pb-2 text-xl'>Captured Text:</h1>
-          <pre>{code}</pre>
-        </div>
-      </div>
-
+      </Link>
     </div>
   );
 }
