8c2aacbcec7538b956dd0c7dc6cb0388687c8491
leo6liu <leo.battalora@gmail.com>
2022-02-25 18:43:03 -0500

    update website title

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -15,7 +15,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "AlgoSim" };
 };
 
 export default function App() {
