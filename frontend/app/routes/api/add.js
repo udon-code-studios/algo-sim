0529fb89a9648183bbfe3e49ccc7ff97262dda55
leo6liu <leo.battalora@gmail.com>
2022-03-28 03:00:25 -0400

    changed add backend from js to go

--- frontend/app/routes/api/add.js
+++ frontend/app/routes/api/add.js
@@ -1,4 +1,4 @@
-const URI = (process.env.BACKEND_URI) ? process.env.BACKEND_URI : 'http://localhost:3001';
+const URI = (process.env.BACKEND_URI) ? process.env.BACKEND_URI : 'http://localhost:8080';
 
 export async function action({ request }) {
   const body = await request.formData();
@@ -8,10 +8,10 @@ export async function action({ request }) {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      a: body._fields.a[0],
-      b: body._fields.b[0],
-      language: body._fields.language[0],
-      code: body._fields.code[0],
+      a: body._fields.a[0], // string
+      b: body._fields.b[0], // string
+      language: body._fields.language[0], // string
+      code: body._fields.code[0], // string
     }),
   });
 
