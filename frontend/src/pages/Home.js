23ce920cc213809864652e0e5a61d93a358bbdb8
leo6liu <leo.battalora@gmail.com>
2022-04-07 23:36:38 -0400

    chartjs

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,62 @@
-export default function PageNotFound() {
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export default function Home() {
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
   return (
-    <div className="flex flex-col justify-center items-center h-80 text-5xl font-bold font-serif">
-      Home
+    <div className="flex flex-col justify-center items-center space-y-10">
+      <pre className='pt-10 text-5xl font-bold font-serif'>Home</pre>
+      <div className="h-80 w-3/4">
+        <Line
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+            },
+          }}
+          data={{
+            labels,
+            datasets: [
+              {
+                label: 'Dataset 1',
+                data: labels.map(() => 3.2),
+                borderColor: '#E2B4BD',
+                backgroundColor: '#E2B4BD80',
+              },
+              {
+                label: 'Dataset 2',
+                data: labels.map(() => 4),
+                borderColor: '#94DBA0',
+                backgroundColor: '#94DBA080',
+              },
+            ],
+          }}
+        />
+      </div>
+
     </div>
   );
 }
