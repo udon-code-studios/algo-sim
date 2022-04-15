775008945492cb68db8a8e3b10d49258d9c63557
leo6liu <leo.battalora@gmail.com>
2022-04-15 03:51:11 -0400

    prepend env variable with REACT_APP_

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URI = (process.env.BACKEND_URI) ? process.env.BACKEND_URI : 'http://localhost:8080';
+export const BACKEND_URI = (process.env.REACT_APP_BACKEND_URI) ? process.env.REACT_APP_BACKEND_URI : 'http://localhost:8080';
