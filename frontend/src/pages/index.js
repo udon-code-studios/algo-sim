e7f7ce12fc2e237448164721ac04565d5dfcc348
leo6liu <leo.battalora@gmail.com>
2022-04-23 00:06:45 -0400

    add AlgoV1 as a page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,5 +3,6 @@ import PageNotFound from "./PageNotFound";
 import Add from "./Add";
 import RawBars from "./RawBars";
 import Graph from "./Graph";
+import AlgoV1 from "./AlgoV1";
 
-export { Home, PageNotFound, Add, RawBars, Graph };
+export { Home, PageNotFound, Add, RawBars, Graph, AlgoV1 };
