4416d190b24960831806b9f218a74512376b8e2a
leo6liu <leo.battalora@gmail.com>
2022-04-23 00:06:34 -0400

    added link to algov1 and changed widths

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,21 +8,27 @@ function Home() {
         Welcome to AlgoSim!
       </h1>
 
-      <Link to="/add">
-        <div className="min-w-[22rem] rounded-xl bg-teal-300 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
+      <Link to="/add" className="w-1/2">
+        <div className="rounded-xl bg-teal-200 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
           Click me to add numbers.
         </div>
       </Link>
 
-      <Link to="/raw-bars">
-        <div className="min-w-[22rem] rounded-xl bg-teal-400 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
+      <Link to="/raw-bars" className="w-1/2">
+        <div className="rounded-xl bg-teal-300 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
           Click me to see raw bars.
         </div>
       </Link>
 
-      <Link to="/graph">
-        <div className="min-w-[22rem] rounded-xl bg-teal-500 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
+      <Link to="/graph" className="w-1/2">
+        <div className="rounded-xl bg-teal-400 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
           Click me to see graphs.
+        </div>
+      </Link>
+
+      <Link to="/algo-v1" className="w-1/2">
+        <div className="rounded-xl bg-teal-500 p-4 text-center text-2xl font-bold shadow-2xl duration-300 hover:bg-teal-900 hover:text-teal-200">
+          AlgoSim version 1.0
         </div>
       </Link>
     </div>
